3b890178426190e89d0a3d1c6b715fd261028ea7
sriharsha1507 <sriharsha1507@gmail.com>
2020-07-06 17:45:49 -0500

    Fixed DI bug in Broadcast receiver by using workaround. #HILT-ALPHA-BUG

--- app/src/main/java/com/developergunda/analyticsarchitecture/AppUpdatesReceiver.kt
+++ app/src/main/java/com/developergunda/analyticsarchitecture/AppUpdatesReceiver.kt
@@ -6,11 +6,23 @@ import android.content.Intent
 import com.developergunda.analyticsarchitecture.analytics.AppEvents
 import com.developergunda.lib.analytics.AnalyticsManager
 import com.developergunda.lib.analytics.AnalyticsService
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.internal.managers.BroadcastReceiverComponentManager
+import dagger.hilt.internal.UnsafeCasts
 import timber.log.Timber
 import javax.inject.Inject
 
-class AppUpdatesReceiver @Inject constructor(private val analyticsService: AnalyticsService) : BroadcastReceiver() {
+@AndroidEntryPoint
+class AppUpdatesReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var analyticsService: AnalyticsService
+
     override fun onReceive(context: Context?, intent: Intent?) {
+        //Workaround for hilt broadcast receiver injection -- Alpha mode : ( -- Still Hilt rocks
+        val injector = BroadcastReceiverComponentManager.generatedComponent(context) as AppUpdatesReceiver_GeneratedInjector
+        injector.injectAppUpdatesReceiver(UnsafeCasts.unsafeCast(this))
+
         intent?.let {
             if (it.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
                 analyticsService.log(AppEvents.AppUpdated())
